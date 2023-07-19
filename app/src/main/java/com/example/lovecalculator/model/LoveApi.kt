eff39ea7fbfb5067b7d2f8ce21ece13483181562
KilluaZ12 <kamalmusaev.12.92@gmail.com>
2023-07-19 23:18:20 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lovecalculator/model/LoveApi.kt
@@ -0,0 +1,19 @@
+package com.example.lovecalculator.model
+
+import com.example.lovecalculator.model.LoveModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface LoveApi {
+
+    @GET("getPercentage")
+    fun getPercentage(
+        @Query("fname") firstName: String,
+        @Query("sname") secondName: String,
+        @Header("X-RapidAPI-Key") key: String = "f9468536bcmsha61c49514b16d75p1fbd05jsn8076c2d94f07",
+        @Header("X-RapidAPI-Host") host: String = "love-calculator.p.rapidapi.com"
+
+    ): Call<LoveModel>
+}
